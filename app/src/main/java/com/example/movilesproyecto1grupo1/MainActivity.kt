7ef4b04dcaf81b3f2e76a8807e50fb9e0726ed76
JamRamirez <jamcarlo99@gmail.com>
2023-04-11 01:36:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/movilesproyecto1grupo1/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.movilesproyecto1grupo1
+
+/* Estudiantes:
+*  Jam Carlos Ramirez Chaves
+*  Marlen Badilla Campos
+*  Fabiana Barrantes Li
+*/
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
